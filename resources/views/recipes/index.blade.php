6b5186d76074462bcbf27c903895577fc77a209f
edonurcahyo <122657252+edonurcahyo@users.noreply.github.com>
2024-12-23 19:20:27 +0700

    Update index.blade.php

--- resources/views/recipes/index.blade.php
+++ resources/views/recipes/index.blade.php
@@ -5,13 +5,16 @@
     <a href="{{ route('recipes.create') }}">Add New Recipe</a>
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
     <ul>
-        @foreach ($recipes as $recipe)
-            <li>
+    @foreach ($recipes as $recipe)
+        <li>
+            @if ($recipe->image)
                 <img src="{{ asset('storage/' . $recipe->image) }}" alt="{{ $recipe->name }}" width="100">
-                <h2>{{ $recipe->name }}</h2>
-                <p>{{ $recipe->description }}</p>
-                <p><strong>Ingredients:</strong> {{ $recipe->ingredients }}</p>
-            </li>
-        @endforeach
-    </ul>
+            @endif
+            <h2>{{ $recipe->name }}</h2>
+            <p>{{ $recipe->description }}</p>
+            <p><strong>Ingredients:</strong> {{ $recipe->ingredients }}</p>
+        </li>
+    @endforeach
+</ul>
+
 @endsection
